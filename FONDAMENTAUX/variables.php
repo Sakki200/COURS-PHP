85a432e81b0eac696e684b7bf84c2444ca618ea1
Sakki <nilkiller200@gmail.com>
2024-07-29 12:58:42 +0200

    MAJ 29/07 13h

--- FONDAMENTAUX/variables.php
+++ FONDAMENTAUX/variables.php
@@ -15,3 +15,14 @@ echo nl2br("ma couleur préféré est le " . COULEUR . PHP_EOL);
 echo nl2br("__LINE__ : " . __LINE__ . PHP_EOL);
 echo nl2br("__DIR__ : " . __DIR__ . PHP_EOL);
 echo nl2br("__FILE__ : " . __FILE__ . PHP_EOL);
+
+
+//VALEURS ET RÉFÉRANCES
+$a = 10;
+$b = $a;
+$c = &$a; //Affectation par référence
+
+$a = 20;
+echo nl2br("a = " . $a . PHP_EOL);
+echo nl2br("b = " . $b . PHP_EOL); //A garder la valeur initial
+echo nl2br("c = " . $c . PHP_EOL); //A pris la valeur changer de a
